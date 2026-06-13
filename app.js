ccf33bbe9c4857e48a479f855dd9308570eb9e68
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2026-06-14 00:05:54 +0800

    set constant port

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const express = require('express');
 
 const Server = new server(express, {
   endpoints: ['src/routes', 'src/features'],
-  port: config.get('server.port') || 3000,
+  // port: config.get('server.port') || 3000,
+  port: 3000,
   host: config.get('server.host'),
   apiBasePath: config.get('server.apiBaseRoot'),
   // v2: config.get('server.apiV2'),
