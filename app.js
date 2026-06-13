32deb48019b053ba91d38e435322e810f1badd2a
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2026-06-14 00:26:47 +0800

    update

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const express = require('express');
 
 const Server = new server(express, {
   endpoints: ['src/routes', 'src/features'],
-  port: config.get('server.port') || 3000,
-  host: config.get('server.host'),
+  // port: config.get('server.port') || 3000,
+  port: 3000,
+  // host: config.get('server.host'),
   apiBasePath: config.get('server.apiBaseRoot'),
   // v2: config.get('server.apiV2'),
 });
